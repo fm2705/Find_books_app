9cffea19f56110baffff7405070cb490ab5591a9
Ludmilla <ludmillajani@yahoo.com>
2023-07-07 17:06:27 -0400

    adding project structure/scalaton

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Find_books_app
+# create react-app
+npx create-react-app my_app
+cd my_app
+npm start
+https://fonts.google.com/
+rafce short
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-   <> </>
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+  </BrowserRouter>
+  
 );
 
