e68aa08b907f4993e3e1c799b0d51eed6dd39fb9
Ludmilla <ludmillajani@yahoo.com>
2023-07-07 22:39:31 -0400

    cleanup

--- README.md
+++
@@ -1,7 +0,0 @@
-# Find_books_app
-# create react-app
-npx create-react-app my_app
-cd my_app
-npm start
-https://fonts.google.com/
-rafce short
--- my_app/src/index.js
+++ my_app/src/index.js
@@ -4,6 +4,12 @@ import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
 import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails";
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
